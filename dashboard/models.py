bf05d5de2973eccf4e8cf02df212eb669e7025e6
Eric <eric.silva@nsprevention.com.br>
2020-03-11 15:37:18 -0300

    Criando docstring das funções

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,7 +9,14 @@ client = MongoClient(config("DATABASE_HOST"), config("DATABASE_PORT", cast=int))
 db = client.dbbot
 
 
-def exist_words(word: str):
+def exist_words(word: str) -> True or False:
+    """
+    Verifica se uma palavra já existe na collection stopwords.
+    :param word: Uma palavra a ser verificada
+    :type word: str
+    :return:True or False
+    :rtype: bool
+    """
     stopwords = db.stopwords
     if stopwords.find_one('words') is None:
         return False
@@ -17,6 +24,11 @@ def exist_words(word: str):
 
 
 def message():
+    """
+    Faz um find na collection message buscando todas as palavras dentro da collection.
+    :return: Retorna a lista contendo todas as mensagens encontradas exceto mensagens já ignoradas.
+    :rtype: list
+    """
     messages = []
     msg = db.message.find()
     for m in msg:
@@ -26,7 +38,7 @@ def message():
         messages.append(message_obj)
     return messages
 
-
+### Preciso trabalhar Stopwords function ###
 def stopwords():
     stop_words = corpus.stopwords.words('portuguese')
     new_words = append_words(stop_words)
@@ -36,7 +48,14 @@ def stopwords():
     return new_words
 
 
-def save_words(element_words: list):
+def save_words(element_words: list) -> True or False:
+    """
+    Salva as palavras na collection de stopwords.
+    :param element_words: Palavra a ser salva.
+    :type element_words: str
+    :return: True or False
+    :rtype: bool
+    """
     stopwords = db.stopwords
     for word in element_words:
         word_obj = word.upper()
