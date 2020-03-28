839b950c3035011f106c44def7ace5eedc86e884
Eric <eric.silva@nsprevention.com.br>
2020-03-28 17:54:48 -0300

    Ajuste da classe de normalização dos dados.

--- dashboard/classes/normalizer.py
+++ dashboard/classes/normalizer.py
@@ -7,14 +7,15 @@ class Normalize:
 
     def normalize_sep(self):
         """
-        Faz o split a partir do caracter Pipe |
+        Faz o split a partir do caracter Pipe ","
         :param line: linha a ser tratada.
         :return: retorna uma lista com dados tratados.
         """
         bag_of_words = []
         list_file = self.file.read().decode('utf-8').splitlines()
-        for line in list_file:
-            line_obj = line.replace('"', '').replace(' ', '').replace('|', ',').replace(';', ',').split(',')
-            bag_of_words.append(line_obj)
+        for element in list_file:
+            word = element.replace('"', '').replace(' ', '').replace("'", "").replace('|', ',').replace(
+                ';', ',').split(',')
+            bag_of_words.append(word)
         return bag_of_words
 
