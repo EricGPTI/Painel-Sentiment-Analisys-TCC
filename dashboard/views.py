86fe3fdc79054e419bcbd6435ed31da87896c094
Eric <eric.silva@nsprevention.com.br>
2020-03-31 15:46:31 -0300

    Ajuste de retorno para impossibilidade de criar wordcloud

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,7 +18,6 @@ def home(request):
 
 def wordcloud(request):
     chat = get_chats()
-
     return render(request, 'wordcloud.html', {'chats': chat[0]})
 
 
@@ -39,24 +38,28 @@ def update(request):
     stop = func_stopwords()
     if request.method == 'POST':
         chat = request.POST.get('chat')
-        messages = message(chat)
+        msgs = message(chat)
         summary = []
-        for m in messages:
+        for m in msgs:
             if m is not None:
                 summary.append(m)
         all_summary = ",".join(s for s in summary)
         stops = stopwords()
-        w_cloud = WordCloud(stopwords=stops, background_color="black").generate(all_summary)
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.imshow(w_cloud, interpolation='bilinear')
-        ax.set_axis_off()
-        plt.tight_layout()
-        if os.path.exists(config('PATH_IMG')) is True:
-            os.remove(config('PATH_IMG'))
-            plt.savefig(config('PATH_IMG'), dpi=300, quality=100, format='jpeg')
-            return wordcloud(request)
-        else:
-            plt.savefig(config('PATH_IMG'), dpi=300, quality=100, format='jpg')
+        try:
+            w_cloud = WordCloud(stopwords=stops, background_color="black").generate(all_summary)
+            fig, ax = plt.subplots(figsize=(10, 6))
+            ax.imshow(w_cloud, interpolation='bilinear')
+            ax.set_axis_off()
+            plt.tight_layout()
+            if os.path.exists(config('PATH_IMG')) is True:
+                os.remove(config('PATH_IMG'))
+                plt.savefig(config('PATH_IMG'), dpi=300, quality=100, format='jpeg')
+                return wordcloud(request)
+            else:
+                plt.savefig(config('PATH_IMG'), dpi=300, quality=100, format='jpg')
+                return wordcloud(request)
+        except ValueError:
+            messages.info(request, 'Não existe quantidade mínima de palavras para este chat.')
             return wordcloud(request)
 
 
